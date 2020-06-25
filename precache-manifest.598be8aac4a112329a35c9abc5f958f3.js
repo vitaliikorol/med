ef6d73b52d1697f4297023b13de8773d85e89374
Vitalii Korol <vitaliikorol@MacBook-Pro.local>
2020-06-25 10:43:50 +0300

    Updates

---
+++ precache-manifest.598be8aac4a112329a35c9abc5f958f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02aece102bf5c91b65b5b687b7813b38",
+    "url": "/index.html"
+  },
+  {
+    "revision": "35b65bc8785a87702631",
+    "url": "/static/css/main.7388d769.chunk.css"
+  },
+  {
+    "revision": "0b254143a729d2787fc0",
+    "url": "/static/js/2.6cb66987.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.6cb66987.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35b65bc8785a87702631",
+    "url": "/static/js/main.adb304cf.chunk.js"
+  },
+  {
+    "revision": "27a22f61b3c7e385cc86",
+    "url": "/static/js/runtime-main.4f31e12d.js"
+  }
+]);
